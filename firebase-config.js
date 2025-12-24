91eb21ee9a6f76fd0dd99ceb58884605d7388d3c
AnkDGiri007 <aniket.giri@nice.com>
2025-12-24 19:52:44 +0530

    Include Firebase config for deployment

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCgXPZrqozPlNbQ-ViqCYt1GkvI3Ro_Ixw",
+    authDomain: "wedding-expenses-f62b6.firebaseapp.com",
+    projectId: "wedding-expenses-f62b6",
+    storageBucket: "wedding-expenses-f62b6.firebasestorage.app",
+    messagingSenderId: "565176416758",
+    appId: "1:565176416758:web:724219358d4f1c521f6fd7",
+    measurementId: "G-FHE7DM81JR"
+};
